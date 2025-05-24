c2cc4d390c0ade9cecdd4ba392b40b594b8123a9
GitHub Action <action@github.com>
2025-05-24 08:44:09 +0000

    Initialize template repository proper
    Set package name to com.github.gicleursurrandom.hotm
    Set project name to HOTM
    Set modid to hotm

--- src/main/java/com/github/gicleursurrandom/hotm/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/gicleursurrandom/hotm/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.gicleursurrandom.hotm.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
